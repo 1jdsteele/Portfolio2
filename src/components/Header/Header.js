4a8731041b11fff1f106ae7be18e8d4dc4258b5a
jdsteele <jdsteelepersonal@gmail.com>
2022-05-03 07:43:55 -0700

    header aligned in horizontal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,14 +8,14 @@ function Header() {
   return (
     <header className="header">
       <div className="headerFlex">
-        <div>
+        {/* <div> */}
           <h1>
             Jake Steele:
           </h1>
           <h2>
             Full Stack Software Engineer. 
           </h2>
-        </div>
+        {/* </div> */}
         <div className="headerImage">
           <img src="/IMG_7484.jpeg" alt="face photo" />
         </div>
