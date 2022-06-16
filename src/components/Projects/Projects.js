0420e5cb0c04408c896c16f6850bd9ea999bf1ac
jdsteele <jdsteelepersonal@gmail.com>
2022-06-16 16:28:06 -0700

    pics for websites, borders for images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,7 +22,9 @@ function Projects() {
               </a>
             </h4>
             <h4>
-            <img src="/RAM.png" alt="RAM electronics concept store screenshot" />
+              <a href="https://ram-electronics.netlify.app/">
+                <img src="/RAM.png" alt="RAM electronics concept store screenshot" />
+              </a>
             </h4>
             <p>An e-commerce store that sells electronics. Made in a group in a one week sprint. Built using full stack MERN.</p>
           </div>
@@ -32,12 +34,22 @@ function Projects() {
                 PokeBattle
               </a>
             </h4>
+            <h4>
+              <a href="https://pokemonbattleapp.netlify.app/">
+                <img src="/poke.png" alt="Pokemon battle app screenshot" />
+              </a>
+            </h4>
             <p>A game where you choose Pokemon to see who will prevail in battle! Do you have what it takes to become a champion? Made with React calling the Pokemon API.</p>
           </div>
           <div>
             <h4>
               <a href="https://1jdsteele.github.io/MyPet">
                 MyPet game
+              </a>
+            </h4>
+            <h4>
+              <a href="https://1jdsteele.github.io/MyPet/">
+                <img src="/myPet.png" alt="myPet screenshot" />
               </a>
             </h4>
             <p>A static site game where you take care of your pet! Made with javascript, HTML, and CSS.</p>
